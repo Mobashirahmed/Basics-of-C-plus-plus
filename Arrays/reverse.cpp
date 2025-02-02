60347f5652e09456a6bcdfe8e8c620d7e4658045
Mobashir <mobashir49@outlook.com>
2025-02-02 21:14:29 +0530

    understood the concept of reversing the elements in an Array

--- Arrays/reverse.cpp
+++ Arrays/reverse.cpp
@@ -3,13 +3,16 @@ using namespace std;
 
 void reverse(int arr[], int n){
 
-    int start = 0;
-    int end = n-1;
+    int start = 0; //setting start to the initial index
+    int end = n-1; // setting end to ending index
 
+    // (start < end) is the working condition for the Iteration  
     while(start <= end){
         swap(arr[start], arr[end]); // swap function C++ ki library mein pre-defined hai
-        start++;
-        end--;
+        start++; // moving start towards end
+        end--; // moving end towards start
+        
+        // Above movements will led to a situation when (start > end) and in this scenario Iteration terminates 
     }
 }
 
@@ -23,13 +26,15 @@ void printArray(int arr[], int n){
 
 int main(){
 
-
+    // initializing array
    int arr[6] = {1, 4, 0, 5, -2, 15};
    int brr[5] = {2, 6, 3, 9, 4};
 
+    // reversing the elements
    reverse(arr, 6);
    reverse(brr, 5);
 
+    // printing the Array
    printArray(arr, 6); 
    printArray(brr, 6); 
 }
